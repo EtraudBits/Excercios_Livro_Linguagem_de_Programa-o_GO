be78d2b10ea1e44ffaeb0386a1ed0f06f772e779
duarteoliveira <du84arte@gmail.com>
2026-01-03 21:04:44 -0300

    servidor simples - responde a mesma mensagem que recebe

---
+++ secao_1.7_um_servidor_web/um_servidor_web.go
@@ -0,0 +1,18 @@
+// server1 é um servidor de "eco" simples que responde com a mesma mensagem que recebe.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler) // cada requisição chama handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler ecoa o componente patch do URL requisitado.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
