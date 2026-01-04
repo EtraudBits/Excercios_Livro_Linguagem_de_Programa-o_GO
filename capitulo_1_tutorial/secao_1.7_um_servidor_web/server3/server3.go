23fbc0de246221b09b33ba8e7ba80ed9633ce363
duarteoliveira <du84arte@gmail.com>
2026-01-03 21:34:34 -0300

    inclusao do server3 -> mais completo

---
+++ capitulo_1_tutorial/secao_1.7_um_servidor_web/server3/server3.go
@@ -0,0 +1,41 @@
+// handler pode inormar os cabeçalhos e os dados de formulario que recebe - tornando o servidor util para inspecionar e depurar requisições HTTP.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler ecoa o componente path do URL requsitado
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+// counter responde com o número de requisições feitas ao servidor
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
